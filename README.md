be7a1d8ab647148bb4729a78645e837e4ab3f8da
michrony <michrony@gmail.com>
2013-08-11 18:39:28 -0400

    Ver. 08/11/2013

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 jtouch
 ======
 
-Very simple json-based touch utility
+Simple json-based touch utility
 
-jtouch path - if ./jtouch.json is present, use date time from there. Otherwise, use current date time
+Usage: jtouch [-jd json date time desc] path
+<br>
+       If desc is present, use date time from there. 
+<br>
+       Otherwise, use current date time
 
 Example of jtouch.json: {"hm" : [ 13, 15 ], "ymd" : [ 13, 8, 30 ]}
 
@@ -13,9 +17,9 @@ mvnjars
 Maven support util
 
 mvnjars -h     - help
-
+<br>
 mvnjars -r     - download jars to local directory using mvn dependency:resolve
-
+<br>
 mvnjars        - list jars specified in ./pom.xml
-
+<br>
 mvnjars -dlib  - copy jars to directory lib 
--- jtouch.py
+++ jtouch.py
@@ -1,26 +1,35 @@
 # jtouch.py
 
-# Very simple json-based touch utility
-# Usage: jtouch <path>
-#        If ./jtouch.json is present, use date ime from there. 
+# Simple json-based touch utility
+# Usage: jtouch [-jd json date time desc] <path>
+#        If desc is present, use date time from there. 
 #        Otherwise, use current date time
 
-# Example of jtouch.json: 
+# Example of desc: 
 # {"hm" : [ 13, 15 ], "ymd" : [ 13, 8, 30 ]}
 
 # Version: 07/31/2013
+# Version: 08/11/2013: added argparse and -jd option
 
 import sys, os, glob, json, time
+import argparse
 from datetime import datetime
 
 #-------------------------------------------------------------------------
 def getCfg(fn):
+  if (fn==None):
+       print "jtouch: set current date"  
+       return ""
+       
+  if (fn!=None and not os.path.exists(fn)):
+       print "jtouch: %s does not exist, set current date" % (fn)  
+       return ""
   try:
        cfg = json.loads(open(fn).read())
   except Exception, e:
-       print "jtouch: set current date"
+       print "jtouch: wrong %s, set current date" % (fn)
        return ""
-  print "jtouch: use %s" % (str(cfg))
+  print "jtouch: use %s %s" % (fn, str(cfg))
 
   return cfg
 #-------------------------------------------------------------------------
@@ -36,19 +45,31 @@ def touch(fn, cfg):
       mi = int(cfg["hm"][1])
       t = datetime(y, m, d, h, mi)
      except Exception, e:
-      print "jtouch: wrong jtouch.json - %s" % (str(e))
+      print "jtouch: wrong descriptor(%s, %s)" % (str(e), str(cfg))
       exit()
 
-   if (not os.path.exists(fn)): open(fn, 'w').close() 
+   if (not os.path.exists(fn)): 
+      try:    open(fn, 'w').close() 
+      except: 
+              print "jtouch: wrong file name(%s)" % (fn)
+              return
+              
    t = time.mktime(t.timetuple())
    os.utime(fn, (t, t))
-   print "jtouch: processed %s" % (fn)
+   print "jtouch: processed(%s)" % (fn)
 
    return
 #-------------------------------------------------------------------------
-if (len(sys.argv)>1): 
-   lst = glob.glob(sys.argv[1])
-   if (len(lst)==0): lst = [sys.argv[1]]
-   cfg = getCfg("./jtouch.json")
-   for fn in lst: touch(fn, cfg)
-else: print "jtouch: wrong call"
+parser = argparse.ArgumentParser()
+parser.add_argument("-jd", type = str, help="json date descriptor")
+parser.add_argument("path", type = str, help="files to process")
+args = vars(parser.parse_args())
+
+files = args["path"]
+cfgn  = args["jd"]
+if (cfgn!=None and not cfgn.endswith(".json")): cfgn = cfgn + ".json"
+cfg = getCfg(cfgn)
+
+lst = glob.glob(files)
+if (len(lst)==0): lst = [files]
+for fn in lst: touch(fn, cfg)
